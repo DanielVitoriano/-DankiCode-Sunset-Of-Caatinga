4257b0cc31ae3416a5769501ee1ab6b7ccc48d7b
Daniel da Costa Vitoriano <daniel.costaht2@gmail.com>
2021-07-07 18:19:48 -0300

    novas sprites

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,8 +7,8 @@ var life = 3;
 var run_speed = 300
 var jump_speed = -1000
 var gravity = 2500
-var dir_fish = Vector2(-1, 0)
-var rot_fish = false
+var dir_fish = Vector2(1, 0)
+var rot_fish = true
 #demais
 var can_move = true
 var can_fire = true
@@ -27,25 +27,35 @@ func Move():
 
 		if is_on_floor() and jump:
 			velocity.y = jump_speed
+		
 		if right:
 			dir_fish = Vector2(1, 0)
 			rot_fish = true
 			velocity.x += run_speed
-			$cat.flip_h = true
+			$cat.flip_h = false
 			$collision.position.x = 10
 			$pos_fish.position.x = 50
 			$pos_fish.rotation = 0
-
-		if left:
+			
+		elif left:
 			dir_fish = Vector2(-1 , 0)
 			rot_fish = false
 			velocity.x -= run_speed
-			$cat.flip_h = false
+			$cat.flip_h = true
 			$collision.position.x = -10
 			$pos_fish.position.x = -50
 			$pos_fish.rotation = - 180
 
+
 func _physics_process(delta):
+	
+	#animações
+	if is_on_floor():
+		$anim.play("idle")
+		
+	elif !is_on_floor():
+		$anim.play("jump")
+	
 	velocity.y += gravity * delta
 	Move()
 	Fire()
@@ -60,5 +70,6 @@ func Fire():
 		get_parent().add_child(fish)
 
 func play_on_enter():
+	$anim.stop()
 	$anim.play("on_enter")
 
